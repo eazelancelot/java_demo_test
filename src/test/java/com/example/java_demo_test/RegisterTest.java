7137c529ba5245503b3589bf4e22b2c2e3484273
eazelancelot <eazelancelot@gmail.com>
2023-05-11 11:00:09 +0800

    commit2

--- src/test/java/com/example/java_demo_test/RegisterTest.java
+++ src/test/java/com/example/java_demo_test/RegisterTest.java
@@ -1,8 +1,18 @@
 package com.example.java_demo_test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.java_demo_test.respository.RegisterDao;
+import com.example.java_demo_test.service.ifs.RegisterService;
 
 @SpringBootTest(classes = JavaDemoTestApplication.class)
 public class RegisterTest {
+	
+	@Autowired
+	private RegisterDao registerDao;
+	
+	@Autowired
+	private RegisterService service;
 
 }
