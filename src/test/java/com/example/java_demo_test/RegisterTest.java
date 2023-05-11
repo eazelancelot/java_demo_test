7da1a5a082149a7cfee174fa019eb7acdacd6eb0
eazelancelot <eazelancelot@gmail.com>
2023-05-11 10:57:29 +0800

    test commit

---
+++ src/test/java/com/example/java_demo_test/RegisterTest.java
@@ -0,0 +1,8 @@
+package com.example.java_demo_test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = JavaDemoTestApplication.class)
+public class RegisterTest {
+
+}
